291e41fb88dd33286770e7ad70c3dde64e0d483d
Dominik Burczynski <Dominik.Burczynski@boc-pl.com>
2019-09-09 11:46:54 +0200

    email and default config

---
+++ migration/migration.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.upload_file_morph (id, upload_file_id, related_id, related_type, field) VALUES (1, 1, 1, 'projects', 'picture');
+INSERT INTO public.upload_file_morph (id, upload_file_id, related_id, related_type, field) VALUES (2, 2, 1, 'filemenus', 'files');
+INSERT INTO public.upload_file (id, name, hash, sha256, ext, mime, size, url, provider, public_id, created_at, updated_at) VALUES (1, 'Example Project Picture.png', '45e8a8ebdc8e4adf9c4ca320100c45da', 'avxM_Rh8AmiapbHS9HCfQdMPYOet46RcYvZho+TQIMw', '.png', 'image/png', '685.50', '/uploads/45e8a8ebdc8e4adf9c4ca320100c45da.png', 'local', NULL, '2019-09-06 08:57:54.954+00', '2019-09-06 08:57:54.999+00');
+INSERT INTO public.upload_file (id, name, hash, sha256, ext, mime, size, url, provider, public_id, created_at, updated_at) VALUES (2, 'flyer_digifof.pdf', 'c37b44b952094a5c8a789e3279d1057c', 'xC41LPLeUEQWAt7OAW7hVg9khBe-eLjngVvu3ucZG34', '.pdf', 'application/pdf', '614.59', '/uploads/c37b44b952094a5c8a789e3279d1057c.pdf', 'local', NULL, '2019-09-06 09:00:30.483+00', '2019-09-06 09:00:30.492+00');
+INSERT INTO public.textmenus (id, content, name, visible, created_at, updated_at) VALUES (1, 'This is example description text being a text type', 'Description', true, '2019-09-06 08:58:50.309+00', '2019-09-06 08:58:50.328+00');
+INSERT INTO public.textmenus (id, content, name, visible, created_at, updated_at) VALUES (2, 'This is example of extract field', 'Extract', true, '2019-09-06 09:04:44.202+00', '2019-09-06 09:04:44.217+00');
+INSERT INTO public.textcards (id, name, content, visible, created_at, updated_at) VALUES (1, 'Description Card', 'This is example description text being a card type', true, '2019-09-06 08:59:44.239+00', '2019-09-06 08:59:44.252+00');
+INSERT INTO public.projects_textmenus__textmenus_projects (id, textmenu_id, project_id) VALUES (1, 1, 1);
+INSERT INTO public.projects_textmenus__textmenus_projects (id, textmenu_id, project_id) VALUES (2, 2, 1);
+INSERT INTO public.projects_textcards__textcards_projects (id, textcard_id, project_id) VALUES (1, 1, 1);
+INSERT INTO public.projects (id, name, affiliation, visible, description, created_at, updated_at) VALUES (1, 'Example Project', 'University of Warsaw', true, 'An example project to show the capibilities of the system', '2019-09-06 08:57:54.877+00', '2019-09-06 09:06:44.09+00');
+INSERT INTO public.htmlpages (id, name, content, visible, created_at, updated_at) VALUES (1, 'Home', 'Welcome to Olive Webportal !', true, '2019-09-06 08:11:14.917+00', '2019-09-06 08:11:14.929+00');
+INSERT INTO public.gridurls (id, name, url, enable, created_at, updated_at) VALUES (1, 'localhost', 'http://localhost:1337/', true, '2019-09-06 08:10:01.608+00', '2019-09-06 08:10:01.632+00');
+INSERT INTO public.grids_gridurls__gridurls_grids (id, gridurl_id, grid_id) VALUES (1, 1, 1);
+INSERT INTO public.grids (id, name, type, visible, created_at, updated_at) VALUES (1, 'Projects', 'Project', true, '2019-09-06 08:09:31.286+00', '2019-09-06 08:09:31.307+00');
+INSERT INTO public.filemenus_projects__projects_filemenus (id, filemenu_id, project_id) VALUES (1, 1, 1);
+INSERT INTO public.filemenus (id, description, name, visible, created_at, updated_at) VALUES (1, 'This is example of file download content type', 'Download', true, '2019-09-06 09:00:30.437+00', '2019-09-06 09:00:30.459+00');
